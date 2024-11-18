6e25ad6cfab80c42536c60b2a5ed2d31e2a75ecd
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-11-18 16:23:15 +0700

    Route set

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RedirectIfLoggedIn from "./services/route-service/RedirectIfLoggedIn";
+import UnauthorizedPage from "./Pages/UnauthorizedPage";
+import ErrorPage from './Pages/ErrorPage';
+import MainPage from './Pages/MainContentPage';
+import LoginPage from './Pages/LoginPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+    <Routes>
+      <Route path="/" element={<RedirectIfLoggedIn />} />
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/auth/Unauthorized" element={<UnauthorizedPage />} />
+
+      {/* <Route element={<ProtectedRoute/>}> */}
+        <Route
+          path="/Console/MainPage"
+          element={<MainPage/>}
+        />
+      {/* </Route> */}
+    </Routes>
+  </Router>
   )
 }
 
