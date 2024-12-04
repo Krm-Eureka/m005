69fffb94960a141f64834ca23c947c79e60d99cf
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-12-04 09:36:01 +0700

    add endpoint axios

--- src/Services/axios.jsx
+++ src/Services/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 /* This code snippet is creating an Axios instance with specific configurations. */
 // eslint-disable-next-line react-refresh/only-export-components
 export default axios.create({
-    baseURL : "http://192.168.10.19:9001/",
+    baseURL : "http://192.168.14.94:9001/",
     // baseURL : import.meta.env.VITE_SERVER_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
