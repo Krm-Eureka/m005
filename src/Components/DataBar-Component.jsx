e69bd383cb94ca670581d0309c476b8c57b81cd4
Witsarut Sanongphan <witsarut_s@eurekadesign.co.th>
2024-12-04 17:54:19 +0700

    .

--- src/Components/DataBar-Component.jsx
+++ src/Components/DataBar-Component.jsx
@@ -36,24 +36,26 @@ const DataBar = ({
           value === "Invalid Date"
             ? "Invalid Date"
             : value === "NOK"
-              ? "NOK"
-              : isError
-                ? "NONE"
-                : isWarning
-                  ? "Invalid_Data"
-                  : value
+            ? "NOK"
+            : isError
+            ? "NONE"
+            : isWarning
+            ? "Invalid_Data"
+            : value
         }
-        className={`cursor-${cursor} w-${wth} p-1.5 bg-gray-50 ${isSuccess
-          ? `bg-green-100 border-green-200`
-          : isWarning
+        className={`cursor-${cursor} w-${wth} p-1.5 bg-gray-50 ${
+          isSuccess
+            ? `bg-green-100 border-green-200`
+            : isWarning
             ? `bg-yellow-100 border-yellow-200`
             : `bg-red-100 border-red-200`
-          } border text-lg border-gray-300 text-gray-900 ${isSuccess
+        } border text-lg border-gray-300 text-gray-900 ${
+          isSuccess
             ? `text-black font-semibold`
             : isWarning
-              ? `text-black font-semibold`
-              : `text-black font-semibold`
-          } rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:text-white dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+            ? `text-black font-semibold`
+            : `text-black font-semibold`
+        } rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:text-white dark:bg-gray-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500`}
         placeholder={title}
         disabled={disable}
       />
